87565c6401e817fb4f068b3f4bfef9dfe4f42d75
Brayan125-boop <freshbeatle@gmail.com>
2026-03-28 14:56:43 -0500

    feat: crear proyecto mvc base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EC_1___BRAYAN_DIAZ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
